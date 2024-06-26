4ec407034c5967dead9f40c0542d030a5f57da5d
Pedro Lima <tsc.pedrolima@gmail.com>
2024-06-26 18:09:29 -0300

    style: update addbook component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import AddBook from "@/components/layout/AddBook";
 import Banner from "@/components/layout/Banner";
 import Header from "@/components/layout/Header";
 
-
 export default async function Home() {
   return (
     <div className="h-screen relative">
--- src/components/layout/AddBook/index.tsx
+++ src/components/layout/AddBook/index.tsx
@@ -1,24 +1,62 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
-import AddBookForm from "./AddBookForm";
+import { Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { BookContext } from "@/contexts/BookContext";
+import { useContext } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { FaPlus } from "react-icons/fa6";
+
+export type FormValues = {
+  title: string;
+  author: string;
+};
 
 export default function AddBook() {
+  const { addBook } = useContext(BookContext);
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    addBook(data);
+  };
+
   return (
-    <div className="absolute bottom-5 right-5">
+    <div className="absolute bottom-5 right-5 rounded-full bg-blue-500 hover:bg-blue-600 w-10 h-10 flex justify-center items-center">
       <Drawer>
-        <DrawerTrigger>Adicionar Livro</DrawerTrigger>
+        <DrawerTrigger>
+          <FaPlus className="text-white text-2xl" />
+        </DrawerTrigger>
         <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-            <AddBookForm />
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center p-3 rounded-lg">
+            <DrawerHeader className="px-40 flex flex-col gap-5">
+              <DrawerTitle className="text-center">Qual é o título e o nome do autor do livro?</DrawerTitle>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-3">
+                  <label htmlFor="title" className="font-semibold text-[#020817]">Título</label>
+                  <input
+                    id="title"
+                    className="text-[#020817] p-2 rounded-lg w-full"
+                    {...register("title", { required: "O campo está em branco" })}
+                  />
+                  {errors.title && <p className="text-[10px] font-light text-red-400">{errors.title.message}</p>}
+                </div>
+                <div className="flex items-center gap-3">
+                  <label htmlFor="author" className="font-semibold text-[#020817]">Autor</label>
+                  <input
+                    id="author"
+                    className="text-[#020817] p-2 rounded-lg w-full"
+                    {...register("author", { required: "O campo está em branco" })}
+                  />
+                  {errors.author && <p className="text-[10px] font-light text-red-400">{errors.author.message}</p>}
+                </div>
+              </div>
+            </DrawerHeader>
+            <DrawerFooter>
+              <Button className="bg-blue-400 hover:bg-blue-500 self-center">Adicionar Livro</Button>
+              <DrawerClose>
+                <Button type="button" variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </form>
         </DrawerContent>
       </Drawer>
     </div>
