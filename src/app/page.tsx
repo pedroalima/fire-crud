783e0981593fae19f56b8dfaf38721fa641da780
Pedro Lima <tsc.pedrolima@gmail.com>
2024-07-05 19:56:31 -0300

    fix: image bug in book search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
 "use client";
 import Banner from "@/components/layout/Banner";
-import SearchBook, { formSchema } from "@/components/layout/SearchBook";
-import { getGoogleBooks } from "@/services/dataAccess/googleBooksAccess";
-import { useState } from "react";
-import { z } from "zod";
-
-export interface BookType {
-  id: string,
-  volumeInfo: {
-    title: string,
-    authors: string[],
-    imageLinks: {
-    smallThumbnail: string,
-    thumbnail: string
-    }
-  }
-}
+import SearchBook from "@/components/layout/SearchBook";
 
 export default function Home() {
-  const [ searchResult, setSearchResult ] = useState<BookType[] | []>([]);
-
-  async function onSubmit(value: z.infer<typeof formSchema>) {
-    setSearchResult(await getGoogleBooks(value.title));
-  }
-
   return (
     <main className="flex flex-col items-centers h-screen relative">
       <Banner />
-      <SearchBook onSubmit={onSubmit} searchResult={searchResult} />
+      <SearchBook />
       <div>
         <h2>Meus Livros</h2>
         {/* <BookList /> */}
--- src/components/layout/SearchBook/index.tsx
+++ src/components/layout/SearchBook/index.tsx
@@ -1,19 +1,33 @@
 "use client";
-import { BookType } from "@/app/page";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { getGoogleBooks } from "@/services/dataAccess/googleBooksAccess";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
+export interface BookType {
+  id: string,
+  volumeInfo: {
+    title: string,
+    authors: string[],
+    imageLinks: {
+    smallThumbnail: string,
+    thumbnail: string
+    }
+  }
+}
 
 export const formSchema = z.object({
   title: z.string()
 });
 
-export default function SearchBook({ onSubmit, searchResult } : { onSubmit: (value: z.infer<typeof formSchema>) => void, searchResult: BookType[] | []}) {
+export default function SearchBook() {
+  const [ searchResult, setSearchResult ] = useState<BookType[] | []>([]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -21,9 +35,9 @@ export default function SearchBook({ onSubmit, searchResult } : { onSubmit: (val
     }
   });
 
-  useEffect(() => {
-
-  }, [searchResult]);
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    setSearchResult(await getGoogleBooks(value.title));
+  }
 
   return (
     <section className="flex flex-col justify-center items-center bg-slate-50 py-20">
@@ -48,16 +62,20 @@ export default function SearchBook({ onSubmit, searchResult } : { onSubmit: (val
         {searchResult && searchResult.map((book: BookType) => (
           <Card key={book.id} className="w-[300px]">
             <CardHeader>
-              <CardTitle>{book.volumeInfo.title}</CardTitle>
+              <CardTitle className="text-lg">{book.volumeInfo.title}</CardTitle>
               {book.volumeInfo.authors && book.volumeInfo.authors.map((item, i) => (
-                <CardDescription key={i}>{item}</CardDescription>
+                <CardDescription key={i}>
+                  <span>{item}</span>
+                </CardDescription>
               ))}
             </CardHeader>
-            <CardContent>
-              {/* <Image src={book.volumeInfo.imageLinks.smallThumbnail && book.volumeInfo.imageLinks.smallThumbnail} width={300} height={300} className="w-full h-full" alt={book.volumeInfo.title} /> */}
+            <CardContent className="flex justify-center w-[300px] h-[250px]">
+              <div className="p-2 w-[150px]">
+                <img src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+              </div>
             </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button>Adicionar</Button>
+            <CardFooter className="">
+              <Button className="w-full">Adicionar</Button>
             </CardFooter>
           </Card>
         ))}
