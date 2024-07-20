650b73436aa3148270393c3a9a03e901af7b7617
Pedro Lima <tsc.pedrolima@gmail.com>
2024-07-20 11:56:16 -0300

    feat: add login with google

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -1,22 +1,41 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { auth } from "@/services/firebaseConfig";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useForm } from "react-hook-form";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export function AuthForm() {
-  const form = useForm();
-  const [ user, loading, error ] = useAuthState(auth);
+  // const form = useForm();
 
-  const handleSubmit = form.handleSubmit ((data) => {
-    console.log(data);
-    console.log(user);
-    console.log(loading);
-    console.log(error);
-  });
+  // const [ user, loading, error ] = useAuthState(auth);
+
+  // const handleSubmit = form.handleSubmit ((data) => {
+  //   console.log(data.email);
+
+  //   try {
+  //     sendSignInLinkToEmail(auth, data.email, {
+  //       url: "http://localhost:3000/auth",
+  //       handleCodeInApp: true
+  //     });
+
+  //     window.localStorage.setItem("emailForSignIn", data.email);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // });
+
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }; 
 
   return (
     <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
@@ -27,15 +46,15 @@ export function AuthForm() {
             <CardDescription>Digite seu email abaixo e enviaremos um link mágico para você fazer login.</CardDescription>
           </CardHeader>
           <CardContent>
-            <form className="space-y-4" onSubmit={handleSubmit}>
-              <div>
+            <form className="space-y-4">
+              {/* <div>
                 <Label htmlFor="email" className="sr-only">
                   Email
                 </Label>
                 <Input id="email" type="email"  placeholder="m@example.com" required {...form.register("email")} />
-              </div>
-              <Button type="submit" className="w-full">
-                Enviar Link Mágico
+              </div> */}
+              <Button type="button" className="w-full" onClick={handleGoogleLogin}>
+                Continuar com Google
               </Button>
             </form>
           </CardContent>
